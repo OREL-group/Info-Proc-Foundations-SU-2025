888609cd1a7b601e41edf74858c4330188e01eb0
JMars8 <109171410+JMars8@users.noreply.github.com>
2025-08-13 00:39:46 -0500

    Add files via upload

--- Project Submissions/JESSE_MARSH/main.py
+++ Project Submissions/JESSE_MARSH/main.py
@@ -1,9 +1,18 @@
+#Sensitive information scanner that can be used to detect sensitive data like SSN, CC numbers, etc
+#Regular expressions are used to detect different information types, see sensitive_patterns.py
+#Scanner is currently single file, which must be hardcoded in below
+
 from scanners.csv_scanner import CsvScanner
 from scanners.excel_scanner import ExcelScanner
 from scanners.txt_scanner import TxtScanner
 
-file_path = "data/Hello.xlsx"
+# File that will be scanned
+# TODO: allow for input of file or folder
+file_path = "data/DLP-Test-State-Data/DLPTEST-State-S.xlsx"
+#outputTxtFile = "output.txt"
 
+# Logic to determine file type
+# TODO: logic to traverse folders and files
 def main():
     if file_path.endswith((".xlsx", ".xls")):
         scanner = ExcelScanner(file_path)
