4e6fb8fb18a5a46a24f0d0d1cea38570dd59d497
JMars8 <109171410+JMars8@users.noreply.github.com>
2025-08-13 09:37:46 -0500

    Update main.py

--- Project Submissions/JESSE_MARSH/main.py
+++ Project Submissions/JESSE_MARSH/main.py
@@ -1,6 +1,6 @@
-#Sensitive information scanner that can be used to detect sensitive data like SSN, CC numbers, etc
-#Regular expressions are used to detect different information types, see sensitive_patterns.py
-#Scanner is currently single file, which must be hardcoded in below
+# Sensitive information scanner that can be used to detect sensitive data like SSN, CC numbers, etc
+# Regular expressions are used to detect different information types, see sensitive_patterns.py
+# Scanner is currently single file, which must be hardcoded in below
 
 from scanners.csv_scanner import CsvScanner
 from scanners.excel_scanner import ExcelScanner
